c2c3cf620d10beeb54360443d8acbe419f2a2259
Edward <s182377@students.aie.edu.au>
2018-10-29 16:40:18 +1100

    added more land, sea and platforms
    started editing player reaction with spikes aswell

---
+++ Assets/Damage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    public string DamageObject = "Trap_Needle";
+    private GameObject gameObjectOfDamage;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gameObjectOfDamage = GameObject.Find(DamageObject);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(transform.position == gameObjectOfDamage.transform.position)
+        {
+            Debug.Log("working");
+        }
+	}
+}
