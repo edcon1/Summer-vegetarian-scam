36ba6a619b10386766164afe02ed161915424a53
Hoson Zhong <hoson_zhong@hotmail.com>
2018-10-30 16:59:02 +1100

    Jump and GravityNodes are added
    To use the node script drag it into an obj, set the collider to trigger and choose an effect.

---
+++ Assets/PowerNode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PowerNode : MonoBehaviour
+{
+    public enum Special
+    {
+        Jump,
+        GravitySwitch
+    }
+    public Special effect;
+
+    private PlayerMovement pScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pScript = other.GetComponent<PlayerMovement>();
+
+        if (effect == Special.Jump)
+            other.GetComponent<Rigidbody>().AddForce(transform.up * pScript.jumpForce * 10, ForceMode.Impulse);
+
+
+        if (effect == Special.GravitySwitch)
+            pScript.GravChange();
+    }
+}
+
+
+
+//[CustomEditor(typeof(PowerNode))]
+//[CanEditMultipleObjects]
+//public class PowerNodeEdit : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        var powerScript = target as PowerNode;
+
+//        powerScript.effect
+
+//        serializedObject.Update();
+//        EditorGUILayout.PropertyField(powerNode);
+//        serializedObject.ApplyModifiedProperties();
+
+//        if (powerNode.S)
+//    }
+//}
