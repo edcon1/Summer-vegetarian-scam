60d9ec1d1c2907765910805a6dd26fc754bc8807
Hoson Zhong <hoson_zhong@hotmail.com>
2018-10-30 15:29:55 +1100

    Fixed both the player and camrera
    Both the player and cameras should be working completely as intended. Camera now able to lock XYZ transforms. Player can now have a set timer for the jump.

---
+++ Assets/LockGlobalTransform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockGlobalTransform : MonoBehaviour
+{
+    public bool lockX = false;
+    public bool lockY = false;
+    public bool lockZ = false;
+
+    private Vector3 startingPos;
+    private Vector3 parentPos;
+    private float newX;
+    private float newY;
+    private float newZ;
+	// Use this for initialization
+	void Start ()
+    {
+        startingPos = new Vector3();
+        startingPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        parentPos = GetComponentInParent<Transform>().position;
+
+        if (lockX)
+            newX = startingPos.x;
+        else
+            newX = parentPos.x;
+
+        if (lockY)
+            newY = startingPos.y;
+        else
+            newY = parentPos.y;
+
+        if (lockZ)
+            newZ = startingPos.z;
+        else
+            newZ = parentPos.z;
+
+        transform.position = new Vector3(newX, newY, newZ);
+    }
+}
