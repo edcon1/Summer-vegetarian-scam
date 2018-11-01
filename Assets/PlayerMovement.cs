262bb0718137fd92c8eaab9a85b13e22492a35c5
Hoson Zhong <hoson_zhong@hotmail.com>
2018-11-01 16:54:52 +1100

    updfate

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,15 +15,20 @@ public class PlayerMovement : MonoBehaviour
     public float fallMagnitude = 2.5f;
     [Range(-100, 0), Tooltip("Strength of gravity, this value must be negative.")]
     public float gravity = -10f;
-    public float testScrollSpeed = 50;
+    [Tooltip("This controls the speed of the character. Positive means it's going RIGHT, negative goes LEFT.")]
+    public float scrollSpeed = 50;
+    [Tooltip("How long the invincibility power up lasts, in seconds.")]
+    public float invincibilityTime = 5.0f;
 
+    public bool isInvincible { get; private set; } 
+    [HideInInspector]
+    public string selectedTag = "Untagged";
     private Rigidbody rb;
     private RaycastHit hit;
     private Vector3 rayDir;
 
     private float distToGround;
     private double collisionOffset = 0.1E-05;
-
     private float jumpTimer = 0;
 
     // Use this for initialization
@@ -31,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
         Physics.gravity = new Vector3 (0, gravity, 0);
         rb = GetComponent<Rigidbody>();
         rayDir = Vector3.down;
+        isInvincible = false;
     }
 	
 	// Update is called once per frame
@@ -45,7 +52,7 @@ public class PlayerMovement : MonoBehaviour
         else
             jumpTimer += Time.deltaTime;
 
-        gameObject.transform.Translate(transform.right * -testScrollSpeed * Time.deltaTime);
+        gameObject.transform.Translate(transform.right * -scrollSpeed * Time.deltaTime);
     }
 
     // From what I read, I should do all the physics stuff in "FixedUpdate", called once a frame
@@ -71,5 +78,49 @@ public class PlayerMovement : MonoBehaviour
             rayDir = Vector3.down;
 
         jumpForce = -jumpForce;
+    }
+
+    public IEnumerator Invincible()
+    {
+        GameObject[] dangers = GameObject.FindGameObjectsWithTag(selectedTag);
+
+        foreach (GameObject d in dangers)
+            Physics.IgnoreCollision(d.GetComponent<Collider>(), GetComponent<Collider>(), true);
+
+        isInvincible = true;
+        yield return new WaitForSeconds(invincibilityTime);
+        isInvincible = false;
+
+        foreach (GameObject d in dangers)
+            Physics.IgnoreCollision(d.GetComponent<Collider>(), GetComponent<Collider>(), false);
+    }
+}
+
+[CustomEditor(typeof(PlayerMovement))]
+[CanEditMultipleObjects]
+public class PMInspector : Editor
+{
+    // Some types are saved by value, not reference, which means that the serialized variable needs to be constantly updated.
+    SerializedProperty serialisedTag;
+
+    private void OnEnable()
+    {
+        // Sets the serialised property to an save existing variable?
+        serialisedTag = serializedObject.FindProperty("selectedTag");
+    }
+
+    public override void OnInspectorGUI()
+    { 
+        serializedObject.Update();
+
+        base.OnInspectorGUI();
+        var pmScript = target as PlayerMovement;
+
+        pmScript.selectedTag = EditorGUILayout.TagField(
+            new GUIContent("Invincibility Tag", "Any object with this tag will not affect players when they're invincible."), 
+            pmScript.selectedTag);
+
+        serialisedTag.stringValue = pmScript.selectedTag;
+        serializedObject.ApplyModifiedProperties();
     }
 }
