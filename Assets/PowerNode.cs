76804040d83a9f9fa55d7d9819ba92d3403100ab
Hoson Zhong <hoson_zhong@hotmail.com>
2018-11-01 11:04:46 +1100

    Toggle Switch and Custom Inspector
    The improved inspector is for the ease of access.

--- Assets/PowerNode.cs
+++ Assets/PowerNode.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(Collider))]
 public class PowerNode : MonoBehaviour
 {
     public enum Special
     {
         Jump,
-        GravitySwitch
+        GravitySwitch,
+        ToggleSwitch
     }
     public Special effect;
+    public float jumpMagnitude = 1;
+    public GameObject toggleObj;
 
     private PlayerMovement pScript;
 
@@ -20,30 +23,37 @@ public class PowerNode : MonoBehaviour
         pScript = other.GetComponent<PlayerMovement>();
 
         if (effect == Special.Jump)
-            other.GetComponent<Rigidbody>().AddForce(transform.up * pScript.jumpForce * 10, ForceMode.Impulse);
-
+            other.GetComponent<Rigidbody>().AddForce(transform.up * pScript.jumpForce * jumpMagnitude, ForceMode.Impulse);
 
         if (effect == Special.GravitySwitch)
             pScript.GravChange();
+
+        if (effect == Special.ToggleSwitch)
+            toggleObj.SetActive(!toggleObj.activeSelf);
     }
 }
 
 
 
-//[CustomEditor(typeof(PowerNode))]
-//[CanEditMultipleObjects]
-//public class PowerNodeEdit : Editor
-//{
-//    public override void OnInspectorGUI()
-//    {
-//        var powerScript = target as PowerNode;
+[CustomEditor(typeof(PowerNode))]
+[CanEditMultipleObjects]
+public class PowerNodeEdit : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Sometimes, you have to cast EditorGuiLayout as the type of the variable you want to display, such as your Enum, GameObject, etc.
+        var pScript = target as PowerNode;
+        pScript.effect = (PowerNode.Special)EditorGUILayout.EnumPopup(
+            new GUIContent("Effect", "This object must have a collider with IS TRIGGER turned ON."),
+            pScript.effect);
 
-//        powerScript.effect
+        if (pScript.effect == PowerNode.Special.Jump)
+            pScript.jumpMagnitude = EditorGUILayout.Slider(
+                new GUIContent("Jump Magnitude", "Multiplies the jump force by this amount. eg. The unchanged jump force is 1, while ten is 10x the unchanged jump force."), 
+                pScript.jumpMagnitude, 1.0f, 10.0f);
 
-//        serializedObject.Update();
-//        EditorGUILayout.PropertyField(powerNode);
-//        serializedObject.ApplyModifiedProperties();
-
-//        if (powerNode.S)
-//    }
-//}
+        if (pScript.effect == PowerNode.Special.ToggleSwitch)
+            pScript.toggleObj = (GameObject)EditorGUILayout.ObjectField(
+                new GUIContent("Toggle Object", "Switch will toggle this object's collider on/off."), pScript.toggleObj, typeof(GameObject), true);
+    }
+}
